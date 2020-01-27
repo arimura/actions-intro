2f61da1de7d7cf72e48da2a7d215fb04af3f6fae
arimura <kotaro_arimura@voyagegroup.com>
2020-01-27 20:17:09 +0900

    ...

---
+++ AndroidApp/app/src/main/java/com/hormiga6/actionsintro/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hormiga6.actionsintro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int t = 0;
+        t = 1;
+    }
+}
